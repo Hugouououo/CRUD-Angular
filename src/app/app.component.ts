fe51da96499761ac3af514c12f6d2049feb9a461
Hugo <hugomoreiralaprano@gmail.com>
2026-07-13 20:52:08 -0300

    Aprendendo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ComponenteUmComponent } from './components/componente-um/componente-um.component';
+import { ComponenteDoisComponent } from './components/componente-dois/componente-dois.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet,
+    // Meus components:
+    ComponenteUmComponent,
+    ComponenteDoisComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
